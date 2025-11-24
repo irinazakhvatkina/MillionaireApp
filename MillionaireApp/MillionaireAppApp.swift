0a050c5947fb6e7b1da2fcb42bda23a199e56a17
Irina <irina.zakhvatkina2307@gmail.com>
2025-11-24 15:16:58 +0500

    first commit

---
+++ MillionaireApp/MillionaireAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireAppApp.swift
+//  MillionaireApp
+//
+//  Created by Administration  on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
