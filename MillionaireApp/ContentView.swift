7102b9fccb43b80b068a190a434b146ecc27bead
Irina <irina.zakhvatkina2307@gmail.com>
2025-11-25 15:40:00 +0500

    feat(home): add basic UI layout for HomeView screen

--- MillionaireApp/ContentView.swift
+++ MillionaireApp/ContentView.swift
@@ -1,24 +1,49 @@
-//
-//  ContentView.swift
-//  MillionaireApp
-//
-//  Created by Administration  on 24/11/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello")
+        ZStack(alignment: .topTrailing) {
+
+            // MARK: - Rulse
+            Button(action: { print("Rules") }) {
+                Image("help")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .foregroundColor(.blue)
+                    .padding()
+            }
+
+            // MARK: - Center Content
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 400, height: 400)
+
+                Text("Who Wants\n to Be a Millionaire?")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -80)
+
+
+                // FIXME: Custom button
+                Button("New Game") { }
+                    .font(.title2)
+                    .padding()
+                    .frame(maxWidth: 240)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+                    .padding(.top, 150)
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
--- MillionaireApp/MillionaireAppApp.swift
+++ MillionaireApp/MillionaireAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  MillionaireAppApp.swift
-//  MillionaireApp
-//
-//  Created by Administration  on 24/11/25.
-//
-
 import SwiftUI
 
 @main
 struct MillionaireAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
